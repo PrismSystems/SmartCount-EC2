87860300419cd8c80cdc5a21a14a91d9b440663a
Terry Reilly <prismbs@gmail.com>
2025-07-28 22:38:58 +0100

    Change access to PDF's

--- components/PdfViewer.tsx
+++ components/PdfViewer.tsx
@@ -1368,6 +1368,7 @@ export const PdfViewer: React.FC<PdfViewerProps> = ({
                 ref={viewerContainerRef}
                 className={`flex-grow w-full h-[calc(100%-40px)] overflow-auto bg-gray-300 shadow-inner relative ${getCursor()}`}
                 onWheel={handleWheel}
+                style={{ touchAction: 'none' }}
             >
                  {isRendering && <div className="absolute inset-0 bg-white/70 flex items-center justify-center z-30"><LoadingIcon/> Rendering PDF...</div>}
                 <div className="relative">
