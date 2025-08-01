18629240f3d50fb4a0b1e599e277a30b1e39e0a0
Terry Reilly <prismbs@gmail.com>
2025-08-01 14:29:41 +0100

    V1.2.0

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
 export const LINEAR_DISCIPLINE_NAME = "Linear";
-export const Version_Number = "1.0.1";
+export const Version_Number = "1.2.0";
 export const App_Tag_Line = "Your takeoff 🚀, accelerated."
 export const App_Name = "Smart Count";
